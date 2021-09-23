354bf1080bd913257e8244abab467bf2857d31cf
0alif <alifhossain003@gmail.com>
2021-09-23 20:18:18 +0600

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = (props) => {
+    const { cart } = props;
+    // const totalReducer = (previous, current) => previous + current.price;
+    // const total = cart.reduce(totalReducer, 0);
+
+    let total = 0;
+    for (const product of cart) {
+        total = total + product.price;
+    }
+    const shipping = 15;
+    const tax = (total + shipping) * 10;
+    const grandTotal = total + shipping + tax;
+
+    return (
+        <div>
+            <h3>Order Summary</h3>
+            <h5>Item Orderd: {props.cart.length}</h5>
+            <br />
+            <p>Total: {total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
+        </div>
+    );
+};
+
+export default Cart;
