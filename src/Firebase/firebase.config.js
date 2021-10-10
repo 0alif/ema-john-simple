29abdc0ad9024624942097fce9f29b1cac43285c
0alif <alifhossain003@gmail.com>
2021-10-10 18:45:01 +0600

    wait

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwNjr7j0IGuEXCYona3z7ZsIH8sMXeby0",
+    authDomain: "ema-jhon-simple-6a2e1.firebaseapp.com",
+    projectId: "ema-jhon-simple-6a2e1",
+    storageBucket: "ema-jhon-simple-6a2e1.appspot.com",
+    messagingSenderId: "177394823158",
+    appId: "1:177394823158:web:05d3c918cf020ec9d3e1d7"
+};
+
+export default firebaseConfig;
